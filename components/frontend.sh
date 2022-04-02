123fff6c93243b98797f3f403298ea7710bdfe28
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-02 22:46:11 +0530

    Description is added.

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+echo -e "\e[36m installing Nginx \e[0m"
 yum install nginx -y
-
+echo -e "\e[36m Downloading Nginx content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
+echo -e "\e[36m Cleanup old nginx content and Extract new download archive file and load confiuration file \e[0m"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
@@ -9,7 +11,7 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
+echo -e "\e[36m starting Nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
