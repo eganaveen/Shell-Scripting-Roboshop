75602a8061ed70431e8957247c3d73cb8a87a2f0
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-02 23:06:55 +0530

    Description is added.

--- components/frontend.sh
+++ components/frontend.sh
@@ -10,9 +10,16 @@ if [ $? -eq 0 ]; then
     echo -e "\e[36m SUCCESS \e[0m"
 else
     echo -e "\e[36m FAILURE \e[0m"
+    exit 1
 fi
 echo -e "\e[36m Downloading Nginx content \e[0m"
 curl  -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zp"
+if [ $? -eq 0 ]; then
+    echo -e "\e[36m SUCCESS \e[0m"
+else
+    echo -e "\e[36m FAILURE \e[0m"
+    exit 1
+fi
 echo -e "\e[36m Cleanup old nginx content and Extract new download archive file and load confiuration file \e[0m"
 cd /usr/share/nginx/html
 rm -rf *
@@ -21,8 +28,20 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ]; then
+    echo -e "\e[36m SUCCESS \e[0m"
+else
+    echo -e "\e[36m FAILURE \e[0m"
+    exit 1
+fi
 echo -e "\e[36m starting Nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
+if [ $? -eq 0 ]; then
+    echo -e "\e[36m SUCCESS \e[0m"
+else
+    echo -e "\e[36m FAILURE \e[0m"
+    exit 1
+fi
 
 
