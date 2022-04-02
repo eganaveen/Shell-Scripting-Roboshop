aa78e335b8c18470133135210d7952b8f3fd6b11
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-02 23:11:39 +0530

    Description is added.

--- components/frontend.sh
+++ components/frontend.sh
@@ -7,15 +7,15 @@ fi
 echo -e "\e[36m installing Nginx \e[0m"
 yum install nginx -y
 if [ $? -eq 0 ]; then
-    echo -e "\e[36m SUCCESS \e[0m"
+    echo -e "\e[32m SUCCESS \e[0m"
 else
     echo -e "\e[31m FAILURE \e[0m"
     exit 1
 fi
 echo -e "\e[36m Downloading Nginx content \e[0m"
-curl  -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zp"
+curl  -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 if [ $? -eq 0 ]; then
-    echo -e "\e[36m SUCCESS \e[0m"
+    echo -e "\e[32m SUCCESS \e[0m"
 else
     echo -e "\e[31m FAILURE \e[0m"
     exit 1
@@ -29,7 +29,7 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 if [ $? -eq 0 ]; then
-    echo -e "\e[36m SUCCESS \e[0m"
+    echo -e "\e[32m SUCCESS \e[0m"
 else
     echo -e "\e[31m FAILURE \e[0m"
     exit 1
@@ -37,7 +37,7 @@ fi
 echo -e "\e[36m starting Nginx \e[0m"
 systemctl restart nginx
 if [ $? -eq 0 ]; then
-    echo -e "\e[36m SUCCESS \e[0m"
+    echo -e "\e[32m SUCCESS \e[0m"
 else
     echo -e "\e[31m FAILURE \e[0m"
     exit 1
