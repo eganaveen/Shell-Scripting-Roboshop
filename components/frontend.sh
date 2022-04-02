60e885f947d44995abdddbdf8c40fec2325324ab
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-03 00:03:52 +0530

    Description is added.

--- components/frontend.sh
+++ components/frontend.sh
@@ -20,20 +20,20 @@ print "installing Nginx"
 yum install nginx -y &>> /tmp/logfile.txt
 statuscheck $?
 print "Downloading Nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> /tmp/logfile.txt
 statuscheck $?
 print "Cleanup old nginx content"
 cd /usr/share/nginx/html
 rm -rf *
 statuscheck $?
 print "Extracting Archive"
-unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . &>> /tmp/logfile.txt
+unzip /tmp/frontend.zip &>> /tmp/logfile.txt && mv frontend-main/* . &>> /tmp/logfile.txt && mv static/* . &>> /tmp/logfile.txt
 statuscheck $?
 print "Update roboshop Configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> /tmp/logfile.txt
 statuscheck $?
 print "starting Nginx"
-systemctl restart nginx && systemctl enable nginx
+systemctl restart nginx &>> /tmp/logfile.txt && systemctl enable nginx &>> /tmp/logfile.txt
 statuscheck $?
 
 
